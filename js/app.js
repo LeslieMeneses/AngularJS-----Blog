cc8542d3505b8520da5586493687fbd6976ab7c0
LeslieMeneses <kathmene27@gmail.com>
2016-03-08 20:49:59 -0500

    Add images

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 var app = angular.module('app',[]);
 app.controller('CtrlPost', function ($scope) {
 	$scope.posts=[
-		{image:"", title:"Tile1", contentSmall:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.", content:"Content1"},
-		{image:"", title:"Tile2", contentSmall:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.", content:"Content2"},
-		{image:"", title:"Tile3", contentSmall:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.", content:"Content3"},
-		{image:"", title:"Tile4", contentSmall:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.", content:"Content4"},
+		{image:"img1", title:"Tile1", contentSmall:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.", content:"Content1"},
+		{image:"img2", title:"Tile2", contentSmall:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.", content:"Content2"},
+		{image:"img3", title:"Tile3", contentSmall:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.", content:"Content3"},
+		{image:"img4", title:"Tile4", contentSmall:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.", content:"Content4"},
 	];
 	$scope.Save=function(){
 		$scope.posts.push({
@@ -19,7 +19,6 @@ app.controller('CtrlPost', function ($scope) {
 	$scope.formVisibility=false;
 	$scope.showForm=function(){
 		$scope.formVisibility=true;
-		console.log($scope.formVisibility);
 	}
 });
 
