3adaf3e40e5a9131d7de879e583abe78c2fbf41a
LeslieMeneses <kathmene27@gmail.com>
2016-03-10 21:40:31 -0500

    The box post is now a directive called boxPost

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var app = angular.module('app',[]);
-app.controller('CtrlPost', function ($scope) {
+
+app.controller('CtrlPost', ['$scope', function ($scope) {
 	$scope.posts=[
 		{image:"img1", title:"Tile1", contentSmall:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.", content:"Content1"},
 		{image:"img2", title:"Tile2", contentSmall:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.", content:"Content2"},
@@ -20,5 +21,17 @@ app.controller('CtrlPost', function ($scope) {
 	$scope.showForm=function(){
 		$scope.formVisibility=true;
 	}
-});
+}]);
 
+app.directive('boxPost', function ($compile) {
+	return {
+		//templateUrl: '../views/box-post.html'
+		//template: '<strong>{{post.contentSmall}}</strong>'
+		link: function (scope, element) {
+			var template = '<div class="box"><div class="image"><img src="../images/{{post.image}}.png" alt=""></div><div class="title">{{post.title}}</div><div class="content-small">{{post.contentSmall}}</div><div><button class="button" >View More</button></div></div><!--END BOX-->';
+			var fnCompile = $compile(template);
+			var content = fnCompile(scope);
+			element.append(content);
+		}
+	};
+});
